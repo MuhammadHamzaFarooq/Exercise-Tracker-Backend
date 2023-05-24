9d4a661ecc7ec81989a3f08436844aff772d4e6f
MuhammadHamzaFarooq <56512212+MuhammadHamzaFarooq@users.noreply.github.com>
2023-05-24 16:04:18 +0100

    bug resolve

--- models/auth/user/Activity.js
+++ models/auth/user/Activity.js
@@ -1,7 +1,7 @@
 import { mongoose, Schema } from "mongoose";
 
 const activitySchema = new mongoose.Schema({
-  user_id: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  user_id: { type: Schema.Types.ObjectId, ref: "User" },
   name: {
     type: String,
     required: true,
